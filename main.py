a8f877c91444c4c560442d48a8cd2da0611ed28f
joselucas257 <69659496+joselucas257@users.noreply.github.com>
2020-08-13 23:05:34 -0300

    main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+#-*-coding:utf8;-*-
+#qpy:console
+
+import time
+import figlet
+print ("BEM VINDO")
+print ("criado por:")
+
+time.sleep (2)
+
+print ("jose lucas")
+time.sleep (2)
+print ("escolha o que vai usar")
+time.sleep (2)
+print ("[1]attack ddos")
+print ("[2]sql injection")
+time.sleep (8)
+print ("==========CAREGANDO===========")
+time.sleep (2)
+print ("[*]••••••                  20%")
+time.sleep (1)
+print ("[*]•••••••••••             50%")
+time.sleep (2)
+print ("[*]••••••••••••••••••      70%")
+time.sleep (3)
+print ("[*]•••••••••••••••••••••• 100%")
+time.sleep (2)
+print ("HACKED BY:jose lucas")
+time.sleep (4)
+print ("copiando todos os dados do aparelho!")
+time.sleep (2)
+print ("rastreando dispositivo!")
+time.sleep (3)
+print ("excluindo todos os arquivos do celular!")
+time.sleep (5)
+print ()
+print ()
+print ()
+print ("CALMA KRAI E PEGADINHA")
+time.sleep (10)
+quit=exit
+exit
+
+
